e12fe7e1fb7bd95aa885b1b5277835236985f506
Mariam <meem7255@gmail.com>
2024-02-03 07:32:35 +0300

    Created a global variables to update the score within the playRound function and compare the values in the playGame function and display it in the console

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function getComputerChoice() {
 	let randomNumber = Math.floor(Math.random() * choices.length);
 	return choices[randomNumber];
 }
+let userScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
 	if (
@@ -10,41 +12,49 @@ function playRound(playerSelection, computerSelection) {
 		(playerSelection === 'rock' && computerSelection === 'scissors') ||
 		(playerSelection === 'paper' && computerSelection === 'rock')
 	) {
-		return `You win! ${playerSelection} beats ${computerSelection}`;
+		return (
+			userScore++, `You win! ${playerSelection} beats ${computerSelection}`
+		);
 	} else if (
 		(computerSelection === 'scissors' && playerSelection === 'paper') ||
 		(computerSelection === 'rock' && playerSelection === 'scissors') ||
 		(computerSelection === 'paper' && playerSelection === 'rock')
 	) {
-		return `You lose! ${computerSelection} beats ${playerSelection}`;
-	} else if (playerSelection === computerSelection) {
-		console.log(
-			`It's a tie! You choose ${playerSelection}, and the computer choose ${computerSelection} too`
+		return (
+			computerScore++, `You lose! ${computerSelection} beats ${playerSelection}`
 		);
+	} else if (playerSelection === computerSelection) {
+		return `It's a tie! You choose ${playerSelection}, and the computer choose ${computerSelection} too`;
 	} else {
 		return 'Invalid input. Please choose rock, paper, or scissors.';
 	}
 }
-const playerSelection = prompt(
-	'choose between rock,paper and scissors'
-).toLocaleLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = prompt(
+// 	'choose between rock,paper and scissors'
+// ).toLocaleLowerCase();
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
 
-// function playGame() {
-// 	let userScore = 0;
-// 	let computerScore = 0;
-// 	for (let i = 1; i <= 5; i++) {
-// 		playRound(playerSelection, computerSelection);
-// 		if (playerSelection > computerSelection) {
-// 			userScore++;
-// 			console.log('u got a point '[i]);
-// 		} else if (computerSelection > playerSelection) {
-// 			computerScore++;
-// 			console.log(`computer got a point`);
-// 		} else {
-// 			console.log(`you got ${userScore} and the computer got ${computerScore}`);
-// 		}
-// 	}
-// }
-// playGame();
+function playGame() {
+	const playerSelection = prompt(
+		'choose between rock,paper and scissors'
+	).toLocaleLowerCase();
+	const computerSelection = getComputerChoice();
+	console.log(playRound(playerSelection, computerSelection));
+	if (computerScore > userScore) {
+		console.log(
+			`computer score: ${computerScore} vs. user score: ${userScore} `
+		);
+	} else if (userScore > computerScore) {
+		console.log(
+			`user score: ${userScore} vs. computer score: ${computerScore} `
+		);
+	} else if (userScore === computerScore) {
+		console.log(
+			`user score: ${userScore} vs. computer score: ${computerScore}`
+		);
+		console.log(typeof userScore, typeof computerScore);
+	}
+}
+// I think the issue is that im comparing a string and not a number
+playGame();
